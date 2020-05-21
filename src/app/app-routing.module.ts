de953fc0c763b879062e34d17428b2e04cb2966d
Manas <pgupta.grd@gmail.com>
2020-05-21 21:38:09 +0530

    created components,routing file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '../app/header/header.component';
+import { ProductComponent } from '../app/product/product.component';
+import { ContactComponent } from '../app/contact/contact.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HeaderComponent },
+  { path: 'product', component: ProductComponent },
+  { path: 'contact-us', component: ContactComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
