148073241e970ce26a447eda0e17fde0e9047099
Pooja Gupta <Pooja.Gupta@ivycomptech.com>
2020-05-25 14:43:36 +0530

    card component

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as cardData from './../../assets/json/card.json';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
-
-  constructor() { }
+  cardData = cardData.data;
+  constructor() { 
+    console.log(this.cardData);
+  }
 
   ngOnInit(): void {
   }
